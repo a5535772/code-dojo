169a8d9cb1725e2665e19f08171e054c16f1f0d4
zhangli <zhangli09@saicmotor.com>
2021-01-06 11:24:53 +0800

    编写了 fizzbuzz的f代码

--- code-dojo-java/src/main/java/com/code/dojo/demo/leetcode412/FizzBuzz.java
+++ code-dojo-java/src/main/java/com/code/dojo/demo/leetcode412/FizzBuzz.java
@@ -1,10 +1,11 @@
 package com.code.dojo.demo.leetcode412;
 
 import java.util.List;
+import com.google.common.annotations.VisibleForTesting;
 
 public class FizzBuzz {
-	public List<String> fizzBuzz(int n) {
+    public List<String> fizzBuzz(int n) {
 
-		return null;
-	}
+        return null;
+    }
 }
